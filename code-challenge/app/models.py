4b5f7017de5ce99b8f09f6b98806be71de9d2229
KittyRobbin <elizabeth.muthoni@student.moringaschool.com>
2024-01-19 14:58:08 +0300

    done

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -38,19 +38,6 @@ class HeroPower(db.Model):
 from flask import Flask
 from flask_migrate import Migrate
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db.init_app(app)
-    migrate = Migrate(app, db)
-
-    with app.app_context():
-        from . import routes, models
-        db.create_all()
-
-    return app
 
 # app/routes.py
 
