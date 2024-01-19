ead8c500e87428ee73056eeabd75e51c59ab9bef
KittyRobbin <elizabeth.muthoni@student.moringaschool.com>
2024-01-19 14:59:08 +0300

    done

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -67,6 +67,19 @@ def get_heroes():
     return jsonify(heroes_data)
 
 # Routes for GET /heroes/:id
+@app.route('/heroes/<int:id>', methods=['GET'])
+def get_hero_by_id(id):
+    hero = Hero.query.get(id)
+    if hero:
+        hero_data = {
+            'id': hero.id,
+            'name': hero.name,
+            'super_name': hero.super_name,
+            'powers': [{'id': power.id, 'name': power.name, 'description': power.description} for power in hero.powers]
+        }
+        return jsonify(hero_data)
+    else:
+        return jsonify({'error': 'Hero not found'}), 404
 
 # Routes for GET /powers
 @app.route('/powers', methods=['GET'])
